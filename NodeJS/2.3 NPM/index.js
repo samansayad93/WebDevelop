dad406b943a1c229dc292af6a6165ae8242f36aa
samansayad93 <94793129+samansayad93@users.noreply.github.com>
2023-08-07 05:53:43 +0330

    NodeJS Huge Commit

---
+++ NodeJS/2.3 NPM/index.js
@@ -0,0 +1,10 @@
+import generateName from "sillyname";
+
+var sillyName = generateName();
+
+console.log(`My name is ${sillyName}.`);
+
+import superheroes from "superheroes";
+
+const name = superheroes.random();
+console.log(`I am ${name}!`);
