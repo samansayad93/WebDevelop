8f89c1a9cc528ae555ce27cb57b4608bee36e805
samansayad93 <sayadhelli10@gmail.com>
2024-12-06 03:39:27 +0330

    Authenitacation

---
+++ Authentication/9.1 Authentication Lv.1/index.js
@@ -0,0 +1,87 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { rateLimit } from "express-rate-limit";
+import pg from "pg";
+import dotenv from "dotenv";
+
+const app = express();
+const port = 3000;
+
+dotenv.config();
+
+const db = new pg.Client({
+  user: process.env.USERR,
+  host: process.env.LOCAL,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+});
+
+app.use(limiter);
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+
+  try{
+    const checkResult = await db.query("SELECT * FROM users WHERE email = $1",[email]);
+
+    if(checkResult.rows.length > 0){
+      res.send("Email already exists. Try logging in.");
+    }else{
+      const result = await db.query("INSERT INTO users (email,password) VALUES ($1, $2)",[email,password]);
+      console.log(result);
+      res.render("secrets.ejs");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+
+  try{
+    const result = await db.query("SELECT * FROM users WHERE email = $1",[email]);
+
+    if(result.rows.length > 0){
+      if(result.rows[0].password === password){
+        res.render("secrets.ejs");
+      } else {
+        res.send("Incorrect Password");
+      }
+    } else {
+      res.send("User Not Found");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
